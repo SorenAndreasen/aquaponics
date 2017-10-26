77c35b6d7a7a60a231e400f8ffbb1d47795148e0
SorenAndreasen <mail@sorenandreasen.com>
2017-10-26 12:53:59 +0200

    windows

--- platform.py
+++ platform.py
@@ -13,10 +13,10 @@ testSyringePosition = 0
 # Pin 21 = G8
 # Pin 22 = G9
 class PlatformController:
-    p1 = Pin('P19', mode=Pin.OUT)
-    p2 = Pin('P20', mode=Pin.OUT)
-    p3 = Pin('P21', mode=Pin.OUT)
-    p4 = Pin('P22', mode=Pin.OUT)
+    p_step = Pin('P19', mode=Pin.OUT)
+    p_dir = Pin('P20', mode=Pin.OUT)
+    #p3 = Pin('P21', mode=Pin.OUT)
+    #p4 = Pin('P22', mode=Pin.OUT)
 
     adc = machine.ADC()
     sensorPin = adc.channel(pin='P13', attn=ADC.ATTN_11DB) #G5 on expansion board
@@ -25,18 +25,22 @@ class PlatformController:
     def __init__(self):
         print("moving")
 
-        self.p4.value(1)
-        time.sleep(5)
-        print("done")
+        self.p_dir.value(1)
+        for num in range(201):
+            self.p_step.value(1)
+            utime.sleep_us(500)
+            self.p_step.value(0)
+            utime.sleep_us(500)
+
 
     def moveToColourAnalysis(self):
         print("moving to colour analysis")
-        if self.currentPosition < colourAnalysisPosition:
-            self.p1.value(1)
-            self.p2.value(0)
-        elif self.currentPosition > colourAnalysisPosition:
-            self.p1.value(0)
-            self.p2.value(1)
+        #if self.currentPosition < colourAnalysisPosition:
+            #self.p1.value(1)
+            #self.p2.value(0)
+        #elif self.currentPosition > colourAnalysisPosition:
+            #self.p1.value(0)
+            #self.p2.value(1)
 
         #while self.currentPosition != colourAnalysisPosition:
             # wait for platform to arrive at destination
@@ -45,12 +49,12 @@ class PlatformController:
 
     def moveToWater(self):
         print("moving to water")
-        if currentPosition < waterPosition:
-            self.p1.value(1)
-            self.p2.value(0)
-        elif currentPosition > waterPosition:
-            self.p1.value(0)
-            self.p2.value(1)
+        #if currentPosition < waterPosition:
+            #self.p1.value(1)
+            #self.p2.value(0)
+        #elif currentPosition > waterPosition:
+            #self.p1.value(0)
+            #self.p2.value(1)
 
         #while self.currentPosition != waterPosition:
             # wait for platform to arrive at destination
@@ -59,12 +63,12 @@ class PlatformController:
 
     def moveToTestSyringe(self):
         print("moving to test syringe")
-        if self.currentPosition < testSyringePosition:
-            self.p1.value(1)
-            self.p2.value(0)
-        elif self.currentPosition > testSyringePosition:
-            self.p1.value(0)
-            self.p2.value(1)
+        #if self.currentPosition < testSyringePosition:
+            #self.p1.value(1)
+            #self.p2.value(0)
+        #elif self.currentPosition > testSyringePosition:
+            #self.p1.value(0)
+            #self.p2.value(1)
 
         #while self.currentPosition != testSyringePosition:
             # wait for platform to arrive at destination
